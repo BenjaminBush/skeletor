49a0252a3aa1d5ed6028eaaa4c07982ea8843b84
Eric Tang <erikhjtang@gmail.com>
2018-11-24 13:00:15 -0600

    1:00 pm Nov24

--- helper/baselines/keras/KerasNAFAgent.py
+++ helper/baselines/keras/KerasNAFAgent.py
@@ -22,23 +22,27 @@ class KerasNAFAgent(KerasAgent):
         V_model = Sequential()
         V_model.add(Flatten(input_shape=(1,) + observation_space.shape,
                         name='FirstFlatten'))
-        V_model.add(Dense(32))
-        V_model.add(Activation('relu'))
-        V_model.add(Dense(32))
-        V_model.add(Activation('relu'))
+        V_model.add(Dense(1024))
+        V_model.add(Activation('sigmoid'))
+        V_model.add(Dense(1024))
+        V_model.add(Activation('sigmoid'))
+        V_model.add(Dense(1024))
+        V_model.add(Activation('sigmoid'))
         V_model.add(Dense(1))
-        V_model.add(Activation('relu'))
+        V_model.add(Activation('sigmoid'))
         print(V_model.summary())
 
         mu_model = Sequential()
         mu_model.add(Flatten(input_shape=(1,) + observation_space.shape,
                          name='FirstFlatten'))
-        mu_model.add(Dense(32))
-        mu_model.add(Activation('relu'))
-        mu_model.add(Dense(32))
-        mu_model.add(Activation('relu'))
+        mu_model.add(Dense(1024))
+        mu_model.add(Activation('sigmoid'))
+        mu_model.add(Dense(1024))
+        mu_model.add(Activation('sigmoid'))
+        mu_model.add(Dense(1024))
+        mu_model.add(Activation('sigmoid'))
         mu_model.add(Dense(action_space.shape[0]))
-        mu_model.add(Activation('relu'))
+        mu_model.add(Activation('sigmoid'))
         print(mu_model.summary())
         
         action_input = Input(shape=(nb_actions,), name='action_input')
@@ -46,29 +50,27 @@ class KerasNAFAgent(KerasAgent):
                               name='observation_input')
         flattened_observation = Flatten()(observation_input)
         x = concatenate([action_input, flattened_observation])
-        x = Dense(32)(x)
-        x = Activation('relu')(x)
-        x = Dense(32)(x)
-        x = Activation('relu')(x)
-        x = Dense(32)(x)
-        x = Activation('relu')(x)
+        x = Dense(1024)(x)
+        x = Activation('sigmoid')(x)
+        x = Dense(1024)(x)
+        x = Activation('sigmoid')(x)
+        x = Dense(1024)(x)
+        x = Activation('sigmoid')(x)
         x = Dense(((nb_actions * nb_actions + nb_actions) // 2))(x)
-        x = Activation('linear', name='L_final')(x)
+        x = Activation('sigmoid', name='L_final')(x)
         L_model = Model(inputs=[action_input, observation_input], outputs=x)
 
 
         # Setup Keras RL's NAFAgent
         memory = SequentialMemory(limit=1000, window_length=1)
-        random_process = OrnsteinUhlenbeckProcess(theta=.15, mu=0., sigma=.3, size=nb_actions)
+        #random_process = OrnsteinUhlenbeckProcess(theta=.15, mu=0., sigma=.3, size=nb_actions)
         self.agent = NAFAgent(nb_actions=nb_actions, V_model=V_model, L_model=L_model, mu_model=mu_model,
-                               memory=memory, nb_steps_warmup=32, random_process=random_process,
+                               memory=memory, nb_steps_warmup=32,
                                gamma=.99, target_model_update=1e-3)   
-        self.agent.compile(Adam(lr=.001))
+        self.agent.compile(Adam(lr=1e-3))
 
 
         self.callbacks = [TensorBoard(log_dir='./tmp/log')]
 
         #self.agent.compile(Adam(lr=.001, clipnorm=1.), metrics=['mae'])
         self.filename = filename
-
-  
